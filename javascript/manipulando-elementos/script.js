9327c7a1b60b4f817cc780e3e2d751e4c19b0592
SamuelsoBarcaro <90662109+SamuelsoBarcaro@users.noreply.github.com>
2022-03-07 22:04:52 -0300

    manipulando javascript aula1

--- javascript/manipulando-elementos/script.js
+++ javascript/manipulando-elementos/script.js
@@ -1,8 +1,26 @@
-const peso = 75
-const altura = 1.70
-const imc = peso / (altura * altura)
-
+//Caputando o elemento p, criando o elemento label dentro de p
 const elementoParagrafo = document.querySelector('p')
-elementoParagrafo.innerText = elementoParagrafo.innerText + imc
+elementoParagrafo.innerHTML = elementoParagrafo.innerText + ' <label> </label>'
+const elementoLabel = document.querySelector('label')
+elementoParagrafo.style.color = 'black'
+elementoLabel.style.color = '#005C78'
 
-console.log(imc)
+
+
+const elementoBotao = document.querySelector('#botao-resultado')
+//Captura do evendo de click do botão
+elementoBotao.onclick = function () {
+    console.log('Fui clicado!')
+
+    const elementoPeso = document.querySelector('#peso')
+    const elementoAltura = document.getElementById('altura')
+    const peso = parseInt(elementoPeso.value)
+    const altura = parseFloat(elementoAltura.value)
+    const imc = peso / (altura * altura)
+
+    if (imc < 18.5) {
+        elementoLabel.innerText = imc.toFixed(2) + ' - Abaixo do peso'
+    } else if (imc > 18.5 && imc < 24.9) {
+        elementoLabel.innerText = imc.toFixed(2) +  ' - Peso normal'
+    }
+}
