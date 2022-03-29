91e8538f6f2b645f19de25890027cffbaac0a688
SamuelsoBarcaro <90662109+SamuelsoBarcaro@users.noreply.github.com>
2022-03-28 22:40:29 -0300

    React

---
+++ React/ola-mundo/src/pages/Home/index.js
@@ -0,0 +1,15 @@
+import "./style.css";
+import Home from "./pages/home"
+
+function Home() {
+    return (
+        <div className="div-home">
+
+            <h1> Esta é minha Página Home </h1>
+
+        </div>
+
+    )
+}
+
+export default Home;
